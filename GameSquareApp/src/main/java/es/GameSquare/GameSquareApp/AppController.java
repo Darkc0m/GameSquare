c3bf0bea194c42d4db0a8d93f1f78955efc27c6f
javierb1984 <55147531+javierb1984@users.noreply.github.com>
2020-02-11 22:19:48 +0100

    index added

--- GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/AppController.java
+++ GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/AppController.java
@@ -1,4 +1,7 @@
 package es.GameSquare.GameSquareApp;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +10,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AppController {
 	
-	@GetMapping("/")
+	@GetMapping("/login")
 	public String session() {
 		return "session";
 	}
+	
 	@GetMapping("/template")
 	public String greeting(Model model, @RequestParam String username, @RequestParam String password) {
 		model.addAttribute("username", username);
 		model.addAttribute("password", password);
 		return "template";
-	}	
+	}
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		List<String> games = Arrays.asList("Nier: Automata","Persona 3","League of Legends");
+		List<String> mods = Arrays.asList("Mod1","Mod2","Mod3");
+		model.addAttribute("games", games);
+		model.addAttribute("mods", mods);
+		return "index";
+	}
 }
 	
--- GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/BBDDController.java
+++ GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/BBDDController.java
@@ -21,20 +21,12 @@ public class BBDDController {
 	@Autowired
 	private ModsRepository ModsRpo;
 	
-	@Autowired
-	private GameDeveloperRepository GameDeveloperRpo;
-	
-	@Autowired
-	private ModDeveloperRepository ModDeveloperRpo;
-	
 	@PostConstruct
 	public void init() {
 		
 		User user1 = new User("Alejuandro-Chan", "Comorobarunaloli13");
 		GameDeveloper user2 = new GameDeveloper("Juez", "Kimochi");
 		ModDeveloper user3 = new ModDeveloper("Javi", "2B9S");
-		GameDeveloper user1developer = new GameDeveloper(user1.getUserName(), user1.getPassword());
-		ModDeveloper user1modder = new ModDeveloper(user1.getUserName(), user1.getPassword());
 		
 		user2.addGame(new Videogame("Lolis", "SliceofLife", "Kawaii", "Juez"));
 		user3.addMod(new Mod("x2", "SliceofLife", "Kawaiix2", "Javi", "Lolis"));
@@ -50,10 +42,6 @@ public class BBDDController {
 		user1.addToCommentList(c1);
 		
 		UsersRpo.save(user1);
-		GameDeveloperRpo.save(user2);
-		ModDeveloperRpo.save(user3);
-		GameDeveloperRpo.save(user1developer);
-		ModDeveloperRpo.save(user1modder);
 		
 		//List<User> users = UsersRpo.findAll();
 	}
