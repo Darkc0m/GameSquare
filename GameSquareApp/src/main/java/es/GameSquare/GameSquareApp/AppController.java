9a8a4f98dceb1e9d3b19e659388e6e7c592f86f8
javierb1984 <55147531+javierb1984@users.noreply.github.com>
2020-02-12 16:37:56 +0100

    cambio de login a link de usuario

--- GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/AppController.java
+++ GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/AppController.java
@@ -3,12 +3,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -21,19 +25,27 @@ public class AppController {
 	private ModsRepository ModsRpo;
 	
 	@GetMapping("/login")
-	public String session() {
+	public String login() {
 		return "session";
 	}
 	
-	@GetMapping("/template")
-	public String greeting(Model model, @RequestParam String username, @RequestParam String password) {
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
+	@PostMapping("/login")
+	public String sumbit(HttpSession session, @RequestParam String username, @RequestParam String password) {
+		session.setAttribute("username", username);
+		session.setAttribute("password", password);
 		return "template";
 	}
 	
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, HttpSession session) {
+		
+		String username = "Login";
+		String user_mapping = "/login";
+		
+		if(session.getAttributeNames().hasMoreElements()) {
+			username = session.getAttribute("username").toString();
+			user_mapping = "/users/"+username;
+		}
 		
 		List<Videogame> games = VideogamesRpo.findFirst10ByOrderByPubDateDesc();
 		List<String> game_names = new ArrayList<String>();
@@ -47,8 +59,21 @@ public class AppController {
 			mod_names.add(mods.get(i).getName());
 		}
 		
+		model.addAttribute("username", username);
+		model.addAttribute("user_mapping", user_mapping);		
 		model.addAttribute("games", game_names);
 		model.addAttribute("mods", mod_names);
+		return "index";
+	}
+	
+	@GetMapping("/game/{param}")
+	public String game(Model model) {
+		return "game";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
 		return "index";
 	}
 }
