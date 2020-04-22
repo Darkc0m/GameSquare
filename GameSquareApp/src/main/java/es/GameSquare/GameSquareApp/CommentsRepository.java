b0281ffd96461322d8f7f194674eb400d49574e6
Darkc0m <52318740+Darkc0m@users.noreply.github.com>
2020-04-22 17:50:55 +0200

    Hazelcast y cache

--- GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/CommentsRepository.java
+++ GameSquareApp/src/main/java/es/GameSquare/GameSquareApp/CommentsRepository.java
@@ -1,13 +1,21 @@
 package es.GameSquare.GameSquareApp;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@CacheConfig(cacheNames="comentarios")
 public interface CommentsRepository extends JpaRepository<Comment, Long>{
 	
+	@CacheEvict(allEntries=true)
+	Comment save(Comment comment);
+	
+	@Cacheable
 	Page<Comment> findByOwnerOrderByPubDateDesc(String owner, Pageable page);
 	
 	
